34a9cfe1d3511f556eee797a1c572df9f91e4214
Azim <yoba.number.one@gmail.com>
2025-11-14 18:20:58 +0300

    feat(tests): adds LinkToken mock

--- apps/contracts/test/mocks/LinkToken.sol
+++ apps/contracts/test/mocks/LinkToken.sol
@@ -12,4 +12,54 @@ interface ERC677Receiver {
     ) external;
 }
 
-contract LinkToken is ERC20 {}
+contract LinkToken is ERC20 {
+    uint256 constant INITIAL_SUPPLY = 1000000000000000000000000;
+    uint8 constant DECIMALS = 18;
+
+    constructor() ERC20("LinkToken", "LINK") {
+        _mint(msg.sender, INITIAL_SUPPLY);
+    }
+
+    function mint(address to, uint256 value) public {
+        _mint(to, value);
+    }
+
+    event Transfer(
+        address indexed from,
+        address indexed to,
+        uint256 value,
+        bytes data
+    );
+
+    function transferAndCall(
+        address _to,
+        uint256 _value,
+        bytes memory _data
+    ) public virtual returns (bool success) {
+        super.transfer(_to, _value);
+
+        emit Transfer(msg.sender, _to, _value, _data);
+        if (isContract(_to)) {
+            contractFallback(_to, _value, _data);
+        }
+
+        return true;
+    }
+
+    function contractFallback(
+        address _to,
+        uint256 _value,
+        bytes memory _data
+    ) private {
+        ERC677Receiver receiver = ERC677Receiver(_to);
+        receiver.onTokenTransfer(msg.sender, _value, _data);
+    }
+
+    function isContract(address _addr) private view returns (bool isContract) {
+        uint256 length;
+        assembly {
+            length := extcodesize(_addr)
+        }
+        return length > 0;
+    }
+}
